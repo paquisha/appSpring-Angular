9cbbf1bca7777860590c20c7ab25eadb5ac25186
paquisha <paquishaxd@gmail.com>
2021-12-21 08:40:00 -0500

    Se agrega primer ejercicio con spring y angular

--- spring-boot-backend-apirest/src/main/java/com/aekmot/springboot/backend/apirest/models/services/IUsuarioService.java
+++
@@ -1,8 +0,0 @@
-package com.aekmot.springboot.backend.apirest.models.services;
-
-import com.aekmot.springboot.backend.apirest.models.entity.Usuario;
-
-public interface IUsuarioService {
-
-	public Usuario findByUsername(String username);
-}
