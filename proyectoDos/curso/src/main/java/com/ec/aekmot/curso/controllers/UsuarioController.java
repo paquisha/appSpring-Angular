71ff4f3100fdf9ef88b68c6874ab1bc85b4dbfc6
aekmot <paquishaxd@gmail.com>
2022-01-02 20:53:23 -0500

    cambios con nuevo front de plantilla de css

---
+++ proyectoDos/curso/src/main/java/com/ec/aekmot/curso/controllers/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.ec.aekmot.curso.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value= "prueba")
+    public String prueba(){
+        return "prueba";
+    }
+}
