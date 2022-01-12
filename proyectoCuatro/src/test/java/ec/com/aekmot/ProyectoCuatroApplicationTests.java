6ae3bb68c92d0ce08921093f1d02a04d189f06c8
paquisha <paquishaxd@gmail.com>
2022-01-12 13:24:46 -0500

    Se agrega ejercicio de repaso spring + react

---
+++ proyectoCuatro/src/test/java/ec/com/aekmot/ProyectoCuatroApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.aekmot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCuatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
