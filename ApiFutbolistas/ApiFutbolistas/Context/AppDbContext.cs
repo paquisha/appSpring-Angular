16ca55d6f91c696b0cf2a485d54eb9095dbb495a
paquisha <paquishaxd@gmail.com>
2022-01-11 20:30:48 -0500

    prueba de creacion de api en c#

---
+++ ApiFutbolistas/ApiFutbolistas/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using ApiFutbolistas.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ApiFutbolistas.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Futbolista> futbolista { get; set; }
+    }
+}
---
+++ ApiFutbolistas/ApiFutbolistas/Models/Futbolista.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFutbolistas.Models
+{
+    public class Futbolista
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string posicion { get; set; }
+        public string nacionalidad { get; set; }
+        public string imagen { get; set; }
+    }
+}
