186eee5627845289e8c9a45bcb8680ba4f006453
paquisha <paquishaxd@gmail.com>
2022-01-14 14:14:32 -0500

    prueba

---
+++ proyectoUno/backend-clientesApp/bin/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Andres", "Grijalva", "mail@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Gina", "Pachacama", "mail1@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Nathalia", "Grijalva", "mail2@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Evelyn", "Benavides", "mail3@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Grace", "Chen", "mail4@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Sandy", "Benavides", "mail5@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Belen", "Garcia", "mail6@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Maria", "Banda", "mail7@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Dolores", "Alcivar", "mail8@mail.com", "2021-12-21");
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES("Alejandra", "Cadena", "mail9@mail.com", "2021-12-21");
