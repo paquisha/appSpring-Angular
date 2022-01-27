a79b5286f04e70b78a33c119cbc352ef8080b696
aekmot <paquishaxd@gmail.com>
2022-01-26 20:40:02 -0500

    Se quita ejercico de angular deprecated

--- proyectoUno/backend-clientesApp/src/main/java/com/aekmot/demo/models/dao/IClienteDao.java
+++
@@ -1,9 +0,0 @@
-package com.aekmot.demo.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.aekmot.demo.models.entity.Cliente;
-
-public interface IClienteDao extends CrudRepository<Cliente, Long>{
-	
-}
