ba2035e2da5579fea31eedfaad9e9872db6f899b
paquisha <paquishaxd@gmail.com>
2022-05-19 19:25:30 -0500

    se agrega ejercicio de tutorial

---
+++ cursoUsuarios/src/main/java/ec/com/aekmot/cursoUsuarios/CursoUsuariosApplication.java
@@ -0,0 +1,13 @@
+package ec.com.aekmot.cursoUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUsuariosApplication.class, args);
+	}
+
+}
