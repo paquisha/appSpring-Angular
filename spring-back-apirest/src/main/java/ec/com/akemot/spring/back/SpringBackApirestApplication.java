e3856a9a5777ffb7ce50a1f7029d636807e221c0
paquisha <paquishaxd@gmail.com>
2022-05-21 23:16:07 -0500

    se agrega ejercio de api rest con spring

---
+++ spring-back-apirest/src/main/java/ec/com/akemot/spring/back/SpringBackApirestApplication.java
@@ -0,0 +1,13 @@
+package ec.com.akemot.spring.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackApirestApplication.class, args);
+	}
+
+}
---
+++ spring-back-apirest/src/test/java/ec/com/akemot/spring/back/SpringBackApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.akemot.spring.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
