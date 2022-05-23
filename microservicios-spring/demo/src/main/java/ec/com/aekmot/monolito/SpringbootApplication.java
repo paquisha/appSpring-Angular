1cb3d38fcc010adb15e12964f541a3079a7ca700
paquisha <paquishaxd@gmail.com>
2022-05-23 15:42:05 -0500

    se agrega ejercicio de front para microservicios

---
+++ microservicios-spring/demo/src/main/java/ec/com/aekmot/monolito/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package ec.com.aekmot.monolito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
