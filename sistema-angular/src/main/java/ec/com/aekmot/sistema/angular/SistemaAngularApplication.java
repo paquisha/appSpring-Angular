1d76710f71c355dacbec6126f263fe1d48132459
paquisha <aekmot@pop-os.localdomain>
2022-05-24 10:39:39 -0500

    se agrega ejercicio con angular

---
+++ sistema-angular/src/main/java/ec/com/aekmot/sistema/angular/SistemaAngularApplication.java
@@ -0,0 +1,13 @@
+package ec.com.aekmot.sistema.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAngularApplication.class, args);
+	}
+
+}
---
+++ sistema-angular/src/test/java/ec/com/aekmot/sistema/angular/SistemaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.aekmot.sistema.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
