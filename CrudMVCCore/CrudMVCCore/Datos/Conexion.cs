abade0bdec40f08cc1605af273f2febef772a1bf
paquisha <paquishaxd@gmail.com>
2022-05-25 19:15:31 -0500

    se agrega parte del ejercicio

---
+++ CrudMVCCore/CrudMVCCore/Datos/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace CrudMVCCore.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSql = string.Empty;
+
+        public Conexion()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            cadenaSql = builder.GetSection("ConnectionStrings:DefaultConection").Value;
+
+        }
+
+        public string getCadenaSql()
+        {
+            return cadenaSql;
+        }
+    }
+}
---
+++ CrudMVCCore/CrudMVCCore/Datos/ContactoDatos.cs
@@ -0,0 +1,36 @@
+﻿using CrudMVCCore.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CrudMVCCore.Datos
+{
+    public class ContactoDatos
+    {
+        public List<Contacto> Listar()
+        {
+            var oLista = new List<Contacto>();
+
+            var cn = new Conexion();
+            using (var connexion = new SqlConnection(cn.getCadenaSql()))
+            {
+                connexion.Open();
+
+                SqlCommand cmd = new SqlCommand("sp_Listar", connexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new Contacto() {
+                            IdContacto = Convert.ToInt32(dr["IdContacto"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            Correo = dr["Correo"].ToString()
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
