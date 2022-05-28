f1b5beb30101728bc8c6a274cc5d04c5a5fd99b0
paquisha <paquishaxd@gmail.com>
2022-05-28 11:03:20 -0500

    se retoma ejercicio de .net mvc

---
+++ CrudMVCCore/CrudMVCCore/Controllers/ContactosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudMVCCore.Controllers
+{
+    public class ContactosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
