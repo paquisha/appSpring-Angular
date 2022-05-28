86bd6e38b6251a91e5594c7b3b6a99f857c3125b
paquisha <paquishaxd@gmail.com>
2022-05-28 12:44:25 -0500

    se temrina ejercicio de crud en mvc con .net core 6

--- CrudMVCCore/CrudMVCCore/Controllers/ContactosController.cs
+++ CrudMVCCore/CrudMVCCore/Controllers/ContactosController.cs
@@ -1,12 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrudMVCCore.Datos;
+using CrudMVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CrudMVCCore.Controllers
 {
     public class ContactosController : Controller
     {
-        public IActionResult Index()
+        ContactoDatos contactoDatos = new ContactoDatos();
+        public IActionResult Listar()
         {
+            //mostrar vista lista contactos
+            var oLista = contactoDatos.Listar();
+            return View(oLista); 
+        }
+
+        public IActionResult Guardar()
+        {
+            //devuelve vista cshtml
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Guardar(Contacto contacto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            //recibe objeto para guardar en el db
+            var respuesta = contactoDatos.Guardar(contacto);
+            if (respuesta)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Editar(int IdContacto)
+        {
+            //METODO SOLO DEVUELVE LA VISTA
+            var ocontacto = contactoDatos.ObtenerPorId(IdContacto);
+            return View(ocontacto);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contacto oContacto)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+
+            var respuesta = contactoDatos.Editar(oContacto);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
+
+        public IActionResult Eliminar(int IdContacto)
+        {
+            //METODO SOLO DEVUELVE LA VISTA
+            var ocontacto = contactoDatos.ObtenerPorId(IdContacto);
+            return View(ocontacto);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(Contacto oContacto)
+        {
+
+            var respuesta = contactoDatos.eliminar(oContacto.IdContacto);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
     }
 }
--- CrudMVCCore/CrudMVCCore/Models/Contacto.cs
+++ CrudMVCCore/CrudMVCCore/Models/Contacto.cs
@@ -1,10 +1,16 @@
-﻿namespace CrudMVCCore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudMVCCore.Models
 {
     public class Contacto
     {
         public int IdContacto { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Telefono es obligatorio")]
         public string? Telefono { get; set; }
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
         public string? Correo { get; set; }
 
     }
