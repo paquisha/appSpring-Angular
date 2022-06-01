b6207995c5589edb02e942c7da982f1bdc35da5d
paquisha <paquishaxd@gmail.com>
2022-05-31 20:54:55 -0500

    api rest con .net

---
+++ ApiAutoresPruebas/ApiAutoresPruebas/Controllers/AutoresController.cs
@@ -0,0 +1,21 @@
+﻿using ApiAutoresPruebas.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAutoresPruebas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Nombre = "Aime" },
+                new Autor(){ Id = 2, Nombre = "Gina"}
+            };
+        }
+    }
+}
