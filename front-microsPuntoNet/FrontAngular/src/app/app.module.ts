8b510a5926246f7e1869d7bd0314f51e9a6c993e
Andres Grijalva <paquishaxd@gmail.com>
2022-09-13 11:07:49 -0500

    Se agrega front en angular

--- front-microsPuntoNet/FrontAngular/src/app/app.module.ts
+++ front-microsPuntoNet/FrontAngular/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductComponent } from './product/product.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'products', component: ProductComponent},
+  {path: 'users', component: UserComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProductComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
