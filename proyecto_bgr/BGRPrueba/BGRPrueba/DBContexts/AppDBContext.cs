7a93194c51ec3eb6c40fa05b8007405ef82a34af
paquisha <paquishaxd@gmail.com>
2023-05-07 20:36:40 -0500

    se agrega prueba para bgr

---
+++ proyecto_bgr/BGRPrueba/BGRPrueba/DBContexts/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using BGRPrueba.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BGRPrueba.DBContexts;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options): base(options)
+    {
+
+    }
+    public DbSet<Persona> Personas { get; set; }
+}
---
+++ proyecto_bgr/BGRPrueba/BGRPrueba/Model/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace BGRPrueba.Model
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string?  lastName { get; set; }
+        public int state { get; set; }
+    }
+}
